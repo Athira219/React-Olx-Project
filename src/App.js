cacdac3199989108ff61e2f8486629066e0f3f24
Athira Somaraj <athirasomaraj070gmail.com>
2022-09-04 12:18:30 +0530

    netflix created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Weather from "./Weather App/Weather";
+import NavBar from "./ComponentsN/NavBar/NavBar";
+import {action, originals,horror, comedy} from './urls'
+import "./App.css";
+import Banner from "./ComponentsN/Banner/Banner";
+import RowPost from "./ComponentsN/RowPost/RowPost";
 
 function App() {
   return (
     <div>
-      <Weather />
+      <NavBar />
+      <Banner />
+      <RowPost url={originals}  title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      
+      <RowPost url={horror} title='Horror' isSmall />
+
     </div>
   );
 }
---
+++ src/Constants/Constants.js
@@ -0,0 +1,3 @@
+export const baseUrl = 'https://api.themoviedb.org/3';
+export const API_KEY = '6332ce0a37a3d434e435e5b085cab47a'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
